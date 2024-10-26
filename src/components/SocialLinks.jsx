8cfde91dc66fcb7eb51853cd67a2d23a3e6a0dde
sandeep <sandeeps16xgmail.com>
2024-10-26 11:11:50 +0530

    Resume Updated

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -39,7 +39,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "/sandeepresume.pdf",
+      href: "/SKResume.pdf",
       style: "rounded-br-md",
       download: true,
     },
